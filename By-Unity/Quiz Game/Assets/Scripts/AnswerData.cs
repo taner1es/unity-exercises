bd62cab1a0d92064803aea290e79d3fcc585fedd
taner1es <taner.esmrglu@gmail.com>
2020-01-05 17:08:47 +0300

    Quiz Game
    this tutorial completed out of git history

---
+++ By-Unity/Quiz Game/Assets/Scripts/AnswerData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AnswerData
+{
+    public string answerText;
+    public bool isCorrect;
+}
---
+++ By-Unity/Quiz Game/Assets/Scripts/MenuScreenController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MenuScreenController : UnityEngine.MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
