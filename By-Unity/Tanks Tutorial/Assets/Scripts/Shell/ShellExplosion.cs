47731fdcdecba5b2a867fc2b623f6dd303bbab1e
taner1es <taner.esmrglu@gmail.com>
2020-01-08 22:42:33 +0300

    Tanks! Tut. Phase 5 - Creating and Firing Shells

--- By-Unity/Tanks Tutorial/Assets/Scripts/Shell/ShellExplosion.cs
+++ By-Unity/Tanks Tutorial/Assets/Scripts/Shell/ShellExplosion.cs
@@ -20,12 +20,51 @@ public class ShellExplosion : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Find all the tanks in an area around the shell and damage them.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+        foreach(Collider col in colliders)
+        {
+            Rigidbody targetRidigbody = col.GetComponent<Rigidbody>();
+
+            if (!targetRidigbody)
+                continue;
+
+            targetRidigbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            TankHealth targetHealth = targetRidigbody.GetComponent<TankHealth>();
+
+            if (!targetHealth)
+                continue;
+
+            float damage = CalculateDamage(targetRidigbody.position);
+
+            targetHealth.TakeDamage(damage);
+        }
+
+        m_ExplosionParticles.transform.parent = null;
+
+        m_ExplosionParticles.Play();
+
+        m_ExplosionAudio.Play();
+
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+        Destroy(gameObject);
     }
 
 
     private float CalculateDamage(Vector3 targetPosition)
     {
         // Calculate the amount of damage a target should take based on it's position.
-        return 0f;
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        float explosionDistance = explosionToTarget.magnitude; //pop 
+
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+
+        float damage = relativeDistance * m_MaxDamage;
+
+        damage = Mathf.Max(0f, damage);
+
+        return damage;
     }
 }
