a549ebc97fd79b7e4f1adbae9104bba153f06777
taner1es <taner.esmrglu@gmail.com>
2020-01-16 13:28:09 +0300

    Brackeys Beginner - Part 2 - add force to player

---
+++ By-Community/Brackeys - Beginner/HowToMakeAVideoGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, Random.Range(100, 1000) * Time.deltaTime);
+    }
+}
