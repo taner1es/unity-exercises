c393d1e487a931b5eb8a4e5423d6cc277d47c1bd
taner1es <taner.esmrglu@gmail.com>
2020-01-16 13:55:20 +0300

    Brackeys Beginner - Part 3 - added physics material to ground to make it slippery - added input states for sideway movements

--- By-Community/Brackeys - Beginner/HowToMakeAVideoGame/Assets/Scripts/PlayerMovement.cs
+++ By-Community/Brackeys - Beginner/HowToMakeAVideoGame/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,19 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, Random.Range(100, 1000) * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
