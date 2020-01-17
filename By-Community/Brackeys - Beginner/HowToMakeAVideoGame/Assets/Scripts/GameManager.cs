76e3c6b90dd22adbb147541e74b5a0366904cdf1
taner1es <taner.esmrglu@gmail.com>
2020-01-17 19:16:03 +0300

    Brackeys Beginner - Part 8 - added, game manager - added, gameover & restart functionality

---
+++ By-Community/Brackeys - Beginner/HowToMakeAVideoGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 2f;
+    public void EndGame()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER!");
+
+            Invoke("Restart", restartDelay);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
