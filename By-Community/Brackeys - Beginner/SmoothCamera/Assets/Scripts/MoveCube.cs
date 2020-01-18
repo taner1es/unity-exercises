c4c22c9e462869e8d3ccf2369d01ab87162e0561
taner1es <taner.esmrglu@gmail.com>
2020-01-18 21:55:12 +0300

    Brackeys Beginner - SmoothCamera - project created - added, smooth camera - added, input handler for cube movement

---
+++ By-Community/Brackeys - Beginner/SmoothCamera/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    float speed = 1000f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.back * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Space))
+            rb.AddForce(Vector3.up * speed * Time.deltaTime);
+        
+    }
+}
