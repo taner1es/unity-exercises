d9342f6313ffd2cbe7e403c4d0d8f90b1bf02864
taner1es <taner.esmrglu@gmail.com>
2020-01-19 22:18:12 +0300

    Brackeys Beginner - AudioManager - project created - added, reusable;singleton audiomanager - added, simple test UI with tiny sound clips

---
+++ By-Community/Brackeys - Beginner/AudioManaging/Assets/Scripts/TestPanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TestPanel : MonoBehaviour
+{
+    public void JumpSFXButtonClicked()
+    {
+        FindObjectOfType<SoundManager>().Play("Jump");
+    }
+
+    public void TouchSFXButtonClicked()
+    {
+        FindObjectOfType<SoundManager>().Play("Touch");
+    }
+
+    public void MenuSceneChangerButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
