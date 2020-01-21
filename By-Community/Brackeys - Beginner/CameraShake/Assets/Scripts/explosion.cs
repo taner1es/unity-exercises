06e63cc2680cd18063e6a602c672b8626f06466e
taner1es <taner.esmrglu@gmail.com>
2020-01-22 02:28:44 +0300

    Brackeys Beginner - Camera Shake - project created - added, curtain, explosion effect, basic clicking script - added, camera shakeing script

---
+++ By-Community/Brackeys - Beginner/CameraShake/Assets/Scripts/explosion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    public GameObject Camera;
+    public float magnitude = .4f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<ParticleSystem>().Play();
+            StartCoroutine(Camera.GetComponent<CameraShake>().Shake(GetComponent<ParticleSystem>().main.duration, magnitude));
+        }       
+    }
+}
