27c51863ceb221c2be54d2153ce7d840b776411c
taner1es <taner.esmrglu@gmail.com>
2020-01-25 17:52:57 +0300

    Brackeys Beginner - Animated Character & Linked Navmesh - project created - imported, lowpolyman model & animation clips & animcontroller - imported, ThirdPersonCharacter script - adjusted, import settings - adjusted, navigation & nav. agent component

---
+++ By-Community/Brackeys - Beginner/Animated Character & Linked Navmesh/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+public class Player : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent navMeshAgent;
+    public ThirdPersonCharacter character;
+
+    private Vector3 destinationPoint;
+
+    private void Start()
+    {
+        navMeshAgent.updateRotation = false;
+    }
+
+    private void Update() {
+        if(Input.GetMouseButton(0)){
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+                destinationPoint = hit.point;
+            }
+        }
+
+        if (navMeshAgent.remainingDistance > .25f)
+        {
+            character.Move(navMeshAgent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
