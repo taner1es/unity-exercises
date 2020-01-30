ad8b2e5b99a9eccf7080d2e4d5296fad12adcee5
taner1es <taner.esmrglu@gmail.com>
2020-01-30 10:38:57 +0300

    Brackeys Beginner - 2D Shooting - project created - imported, sunnyland forest art asset package (environment) - imported, hero and opponents art asset package (player&enemy) - imported, cinemachine package - created, test level - created, reusable bullet (instanciating) - created, reusable bullet (raycasting) - created, reusable player & enemy - created, several animations (bullet explosion; enemy died; archer attack,jump,run) & animation state machine

---
+++ By-Community/Brackeys - Beginner/2D Shooting/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject deathEffect;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ By-Community/Brackeys - Beginner/2D Shooting/Assets/Scripts/Weapon_Raycasting.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Weapon_Raycasting : MonoBehaviour
+{
+    public Transform firePoint;
+    public int damage = 40;
+    public GameObject impactEffect;
+    public LineRenderer lineRenderer;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    private IEnumerator Shoot()
+    {
+        gameObject.GetComponent<Animator>().SetBool("Attack", true);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+        if (hitInfo)
+        {
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            Destroy(Instantiate(impactEffect, hitInfo.point, Quaternion.identity), .2f);
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+
+        lineRenderer.enabled = true;
+
+        yield return new WaitForSeconds(.1f);
+
+        lineRenderer.enabled = false;
+
+        yield return new WaitForSeconds(.1f);
+        gameObject.GetComponent<Animator>().SetBool("Attack", false);
+    }
+}
