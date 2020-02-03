53d3ed3ca544c0540737806c12fc2b890723ee94
taner1es <taner.esmrglu@gmail.com>
2020-02-03 18:57:02 +0300

    Brackeys Beginner - Questing System - project created - created, UI (PlayerWindow & QuestWindow) - created, scripts for questing system and UI handling - adjusted, components - added, sample quest

---
+++ By-Community/Brackeys - Beginner/Questing System/Assets/Scripts/DisplayPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayPlayer : MonoBehaviour
+{
+    public Text healthText;
+    public Text experienceText;
+    public Text goldText;
+
+    internal void UpdateInfos(int newHealth, int newExperience, int newGold)
+    {
+        healthText.text = newHealth.ToString();
+        experienceText.text = newExperience.ToString();
+        goldText.text = newGold.ToString();
+    }
+}
