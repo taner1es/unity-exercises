17ab3fba31558c8e4be0e9b9d2689e3b06781515
taner1es <taner.esmrglu@gmail.com>
2020-02-06 21:10:57 +0300

    Brackeys Beginner - TopDown Movement - project created - imported, tiny rpg - forest (free 2d art set) - created, tile palette - designed, sample scene - created, script for movement - created, animation for movement - adjusted, animation blend tree

---
+++ By-Community/Brackeys - Beginner/TopDown Movement/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+    
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
