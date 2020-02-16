0384906005b13567a50f0dcf6d321843337e046c
taner1es <taner.esmrglu@gmail.com>
2020-02-16 17:03:03 +0300

    Brackeys Beginner - Melee Combat 2D - project created - imported, Bandits - Pixel Art Asset - created, Player can only move(Right&Left) - created, Enemy can be hit - added, attacking feature to Player

---
+++ By-Community/Brackeys - Beginner/Melee Combat 2D/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int maxHealth = 100;
+    int currentHealth;
+
+    Animator animator;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+    }
+
+    internal void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        animator.SetTrigger("Hurt");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy Died");
+
+        animator.SetBool("isDead", true);
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        this.enabled = false;
+    }
+}
