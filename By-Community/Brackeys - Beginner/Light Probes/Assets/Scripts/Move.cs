7cbbf4ef99490f8183b4260adfd6b4d35bb82221
taner1es <taner.esmrglu@gmail.com>
2020-02-24 16:26:49 +0300

    Brackeys Beginner - Light Probes - project created - created, sample stage - adjusted, stage objects converted to static object type - added, light probes - baked, scene light - added, tiny post-processing fx - added, simple movement script to test

---
+++ By-Community/Brackeys - Beginner/Light Probes/Assets/Scripts/Move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 dir;
+
+    [SerializeField] float acceleration = 20f;
+    [SerializeField] float deceleration = 50f;
+
+    [SerializeField] float speed = 2000f; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        dir.x = Input.GetAxis("Horizontal");    
+        dir.z = Input.GetAxis("Vertical");
+        
+        if(dir.sqrMagnitude > Mathf.Epsilon)
+        {
+            dir.Normalize();
+            dir *= Time.deltaTime;
+
+            rb.velocity = Vector3.Lerp(rb.velocity, dir * speed, 1 / acceleration);
+        }
+        else
+        {
+            rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, 1 / deceleration);
+        }
+    }
+}
