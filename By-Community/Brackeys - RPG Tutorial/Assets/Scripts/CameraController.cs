e76d46e31b23b2faa63b4ef565d91d3f69934a6c
taner1es <taner.esmrglu@gmail.com>
2020-02-27 08:16:16 +0300

    Brackeys - RPG in Unity (E01) *Movement* - project created - imported, environment asset - tweaked, lightning settings - created, player object - adjusted, environment gameobjects settings for navigation - baked, navmesh - added, script for player to follow clicked point - added, script for camera to follow the player & zoom in-out & rotation

---
+++ By-Community/Brackeys - RPG Tutorial/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float zoomSpeed = 4f;
+    [SerializeField] private float minZoom = 5f;
+    [SerializeField] private float maxZoom = 15f;
+
+    [SerializeField] private float pitch = 2f;
+
+    [SerializeField] private float currentZoom = 10f;
+    
+    [SerializeField] private float yawSpeed = 100f;
+    [SerializeField] private float currentYaw = 0f;
+    
+    private void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
+    }
+}
---
+++ By-Community/Brackeys - RPG Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private LayerMask movementMask;
+    
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        // Left mouse click event
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+
+        }
+
+        // Right mouse click event
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                   
+            }
+
+        }
+    }
+}
