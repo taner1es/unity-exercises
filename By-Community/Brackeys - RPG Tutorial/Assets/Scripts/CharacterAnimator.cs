d056fdd8cd70868802520d2e97a58dfd97a69b9a
taner1es <taner.esmrglu@gmail.com>
2020-03-01 19:51:02 +0300

    Brackeys - RPG in Unity (E03) *INTEGRATION 01* - project version downgraded, from 2019.3.2f1 to 2019.2.21f1; because of the bug which is happening while importing animations from .blend and .fbx files - imported, character model and animations(idle, walk, run) into unity project - created, script to control the animations

---
+++ By-Community/Brackeys - RPG Tutorial/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    const float locomotionAnimationSmoothTime = .1f;
+
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+    }
+}
