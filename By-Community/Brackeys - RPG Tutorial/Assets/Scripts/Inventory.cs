43b00d523523f104da572787bfae8916783d4f71
taner1es <taner.esmrglu@gmail.com>
2020-03-02 08:37:08 +0300

    Brackeys - RPG in Unity (E04) *ITEMS* - created, item object type - created, inventory: managed on a singleton - added, item picking up functionality - created, sample item object - tested, picking up functionality

---
+++ By-Community/Brackeys - RPG Tutorial/Assets/Scripts/Inventory.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("More than one inventory found");
+            return;
+        }
+
+        instance = this;
+    }
+    #endregion
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallBack;
+
+    public List<Item> items = new List<Item>();
+    public int space = 20;
+
+    internal bool Add(Item item)
+    {
+        if (!item.isDefaultItem)
+        {
+            if(items.Count >= space)
+            {
+                Debug.LogWarning("Not enough space in inventory");
+                return false;
+            }
+
+            items.Add(item);
+
+            if(onItemChangedCallBack != null)
+                onItemChangedCallBack.Invoke();
+        }
+
+        return true;
+    }
+
+    internal void Remove(Item item)
+    {
+        items.Remove(item);
+
+        if (onItemChangedCallBack != null)
+            onItemChangedCallBack.Invoke();
+    }
+}
