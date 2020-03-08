50103f95d1c34f24ceae7560b67d9fe4550d2064
taner1es <taner.esmrglu@gmail.com>
2020-03-08 15:20:04 +0300

    Brackeys - RPG in Unity (E09) *STATS* - created, stat component relative to equipped items - created, player stats

---
+++ By-Community/Brackeys - RPG Tutorial/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChangedCallback += OnEquipmentChanged;
+    }
+
+    private void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        if(newItem != null)
+        {
+            armor.AddModifier(newItem.armorModifier);
+            damage.AddModifier(newItem.damageModifier);
+        }
+
+        if(oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.armorModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
+        }
+    }
+}
---
+++ By-Community/Brackeys - RPG Tutorial/Assets/Scripts/Stats/Stat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat
+{
+    [SerializeField]
+    private int baseValue;
+
+    private List<int> modifiers = new List<int>();
+    public int GetValue()
+    {
+        int finalValue = 0;
+        modifiers.ForEach(x => finalValue += x);
+        return finalValue;
+    }
+
+    public void AddModifier(int modifier)
+    {
+        if (modifier != 0)
+            modifiers.Add(modifier);
+    }
+
+    public void RemoveModifier(int modifier)
+    {
+        if (modifier != 0)
+            modifiers.Add(modifier);
+    }
+}
