562664cee8b86c5677c2da1b0b83c57f7f37ea8f
taner1es <taner.esmrglu@gmail.com>
2020-03-26 00:25:58 +0300

    Brackeys FPS Tutorials - Shooting with Raycast - project created - imported, arena model - modified, arena model UVs&Materials - imported, fps player prefab from previously completed tutorial - added, gun & crosshair - created, target component - created & implemented, muzzle flash particle fx - created & implemented, impact particle fx - applied, force on hit - implemented, auto-fire

---
+++ By-Community/Brackeys - FPS Tutorial/Shooting with Raycast/Assets/Scripts/Target.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
