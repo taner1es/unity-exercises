a50e358463917f5fdb92338cd0ff06891b91320a
taner1es <taner.esmrglu@gmail.com>
2020-03-27 09:40:31 +0300

    Brackeys FPS Tutorials - Ammo & Reloading - project created - imported, previously finished tutorial-project ::Brackeys FPS Tutorials/Weapon Switching:: to continue to work through on - added, reloading functionality - added, idle & reload animation

---
+++ By-Community/Brackeys - FPS Tutorial/Ammo & Reloading/Assets/Scripts/Gun.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public Animator animator;
+
+    private float nextTimeToFire = 0f;
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    private void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
+
+    void Update()
+    {
+        if (isReloading)
+            return;
+
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1 / fireRate;
+            Shoot();
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading..");
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+        animator.SetBool("Reloading", false);
+
+        yield return new WaitForSeconds(.25f);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
+    private void Shoot()
+    {
+        muzzleFlash.Play();
+
+        currentAmmo--;
+
+        RaycastHit hit;
+        if(Physics.Raycast(new Ray(fpsCam.transform.position, fpsCam.transform.forward), out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+    }
+}
