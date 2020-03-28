c75a1a2d4d17bd26b869310f52e68355f65d841a
taner1es <taner.esmrglu@gmail.com>
2020-03-28 10:45:01 +0300

    Brackeys FPS Tutorials - Grenade - Bomb - project created - modelled, a grenade in blender - imported, previously finished tutorial-project ::Brackeys Intermediate/SHATTER - DESTRUCTION:: to continue to work through on - designed, a sample scene with destructibles - imported, ::unity particle pack:: from assetstore (::BigExplosion:: particle fx used) - implemented, explosion fx & logic to grenade - added, triggering to destroy the nearby breakable objects & add force on to them - added, throwing grenade on left mouse click

---
+++ By-Community/Brackeys - FPS Tutorial/Grenade - Bomb/Assets/Scripts/Grenade.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float delay = 3f;
+    public float explosionRadius = 5f;
+    public float explosionForce = 700f;
+
+    public GameObject explosionEffect;
+
+    float countdown;
+    bool hasExploded = false;
+
+    private void Start()
+    {
+        countdown = delay;
+    }
+
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if(countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    private void Explode()
+    {
+        Debug.Log("BOOM!");
+
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach(Collider nearbyObject in collidersToDestroy)
+        {
+            Destructible dest = nearbyObject.GetComponent<Destructible>();
+            if(dest != null)
+            {
+                dest.Destroy();
+            }
+        }
+
+        Collider[] collidersToMove = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach(Collider nearbyObject in collidersToMove)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+        Gizmos.DrawSphere(transform.position, explosionRadius);
+    }
+}
---
+++ By-Community/Brackeys - FPS Tutorial/Grenade - Bomb/Assets/Scripts/GrenadeThrower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class GrenadeThrower : MonoBehaviour
+{
+    public float throwForce = 40f;
+    public GameObject grenadePrefab;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ThrowGrenade();
+        }
+    }
+
+    private void ThrowGrenade()
+    {
+        GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+    }
+}
