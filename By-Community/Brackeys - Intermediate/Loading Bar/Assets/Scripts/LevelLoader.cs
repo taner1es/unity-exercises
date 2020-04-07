ff719f695e4000c8461a15c3542f24d7fe6ab56a
taner1es <taner.esmrglu@gmail.com>
2020-04-07 15:47:32 +0300

    Brackeys Intermediate - Loading Bar - project created - created, scene1 & load level button - created, scene2 to load - implemented, loading bar updates asynchronously

---
+++ By-Community/Brackeys - Intermediate/Loading Bar/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    public Scrollbar LoadingBar;
+    public Text LoadingPercentage;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            LoadingBar.size = operation.progress; 
+            LoadingPercentage.text = operation.progress * 100f + 10f + "%";
+            Debug.Log(progress);
+
+            yield return null;
+        }
+    }
+}
