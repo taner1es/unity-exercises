22b2db1732af3131056fcc3d22b26370ed948337
taner1es <taner.esmrglu@gmail.com>
2020-04-07 18:06:33 +0300

    Brackeys Intermediate - Scriptible Object - project created - drawed, card template - added, sample dragons - created, scriptible object type - created, cards

---
+++ By-Community/Brackeys - Intermediate/Scriptible Objects/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public Text no;
+    public Text name;
+
+    public RawImage avatarImage;
+
+    public Text fire;
+    public Text earth;
+    public Text aqua;
+
+    public Text description;
+
+    private void Start()
+    {
+        no.text = card.no.ToString();
+        name.text = card.name;
+
+        avatarImage.texture = card.avatar.texture;
+
+        fire.text = card.fire.ToString();
+        earth.text = card.earth.ToString();
+        aqua.text = card.aqua.ToString();
+
+        description.text = card.description;
+    }
+}
