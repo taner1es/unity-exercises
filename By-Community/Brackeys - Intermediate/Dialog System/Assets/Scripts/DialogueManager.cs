eaf5919eedeb0bcceee7a8e8b78a6ba48cb12c15
taner1es <taner.esmrglu@gmail.com>
2020-04-07 22:02:20 +0300

    Brackeys Intermediate - Dialog System - project created - imported, special font (chargen) - created, test button & dialog box by using standard UI components - added, dialogue manager&trigger - implemented, dialogue system (used .NET / Queue<T> class to manage the dialogues) - binded, UI elements to dialogue system - added, animator to dialogue box (states:Open/Close) - added, coroutine to type sentences letter by letter on each frame update

---
+++ By-Community/Brackeys - Intermediate/Dialog System/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator dialogueBoxAnimator;
+
+    private Queue<string> sentences;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    internal void StartDialogue(Dialogue dialogue)
+    {
+        dialogueBoxAnimator.SetBool("isOpen", true);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    private void EndDialogue()
+    {
+        dialogueBoxAnimator.SetBool("isOpen", false);
+    }
+}
