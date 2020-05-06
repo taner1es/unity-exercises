83dadae4154e8cf46ba6293d25162fbfb7889dcd
taner1es <taner.esmrglu@gmail.com>
2020-05-06 23:19:21 +0300

    Brackeys Intermediate - 2D Pathfinding - Enemy AI - project created - imported, a previously completed tutorial project ::Brackeys - Beginner/2D Movement:: to have a player - imported, A* free version unity package (https://arongranberg.com/astar/download) - adjusted, added some more obstacles to scene - added, enemy only graphic - created, A* and implemented pathfinder component into - tweaked, pathfinder component settings to compute on 2D - added, AIPath component to enemy object to have the enemy logic - added, a script(EnemyGFX.cs) on enemy to flip the sprite through x axis depends on desired velocity - tested, Enemy - created, duplication of the enemy object and deleted ai related components except Seeker to create a new custom AI by using the A* API with a brand new freshy script(EnemyAI.cs) - implemented & tested, new custom AI script

---
+++ By-Community/Brackeys - Intermediate/2D Pathfinding  - Enemy AI/Assets/2D Movement/Scripts/CharacterMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+    float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
