2924b33ae446d42b46986381a77cca97e7389236
taner1es <taner.esmrglu@gmail.com>
2020-05-09 19:55:20 +0300

    Brackeys Intermediate - Rewind Time - project created - imported, fps character from previously finished tutorial project - imported, grenade & destroyable objects(crate & bottle) from previously finished tutorial project - created, point in time class & time body script - adjusted, max rewindin time memory goes for 5 seconds

---
+++ By-Community/Brackeys - Intermediate/Rewind Time/Assets/Grenade - Bomb/Scripts/GrenadeThrower.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class GrenadeThrower : MonoBehaviour
+{
+    public float throwForce = 40f;
+    public GameObject grenadePrefab;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ThrowGrenade();
+        }
+    }
+
+    private void ThrowGrenade()
+    {
+        GameObject grenade = Instantiate(grenadePrefab, transform.position + transform.forward , transform.rotation);
+
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+
+        //rb.AddForce(transform.InverseTransformDirection(transform.forward) * throwForce);
+
+        rb.transform.position = transform.position + Camera.main.transform.forward;
+        rb.AddForce(Camera.main.transform.forward * throwForce, ForceMode.Acceleration);
+    }
+}
---
+++ By-Community/Brackeys - Intermediate/Rewind Time/Assets/Scripts/TimeBody.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBody : MonoBehaviour
+{
+    bool isRewinding = false;
+
+    float recordTime = 5f;
+
+    List<PointInTime> pointsInTime;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        pointsInTime = new List<PointInTime>();
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartRewind();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            StopRewind();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRewinding)
+            Rewind();
+        else
+            Record();
+    }
+
+    private void Rewind()
+    {
+        if (pointsInTime.Count > 0)
+        {
+            PointInTime pointInTime = pointsInTime[0];
+            transform.position = pointInTime.position;
+            transform.rotation = pointInTime.rotation;
+            pointsInTime.RemoveAt(0);
+        }
+        Debug.Log(pointsInTime.Count);
+    }
+
+    private void Record()
+    {
+        if(pointsInTime.Count > Mathf.Round(recordTime / Time.fixedDeltaTime))
+        {
+            pointsInTime.RemoveAt(pointsInTime.Count - 1);
+        }
+
+        pointsInTime.Insert(0, new PointInTime(transform.position, transform.rotation));
+        Debug.Log(pointsInTime.Count);
+    }
+
+    private void StartRewind()
+    {
+        isRewinding = true;
+        if(rb != null) rb.isKinematic = true;
+    }
+
+    private void StopRewind()
+    {
+        isRewinding = false;
+        if(rb != null) rb.isKinematic = false;
+    }
+
+}
