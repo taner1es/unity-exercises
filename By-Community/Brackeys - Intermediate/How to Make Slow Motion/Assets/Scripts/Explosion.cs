944f1ea4a2f0a02c7f90487126e158bde28da5c5
taner1es <taner.esmrglu@gmail.com>
2020-05-10 00:27:36 +0300

    Brackeys Intermediate - How to Make Slow Motion - project created - imported, fps character from previously finished tutorial project - imported, grenade & destroyable objects(crate & bottle) from previously finished tutorial project - created, time manager - imported, a new tiny explosion effect (removed the one which is imported with grenade package because of size) - created, a simple explosion gun - trigged, slow motion fx through explosion gun

---
+++ By-Community/Brackeys - Intermediate/How to Make Slow Motion/Assets/Scripts/Explosion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float explosionRadius = 5f;
+    public float explosionForce = 700f;
+
+    public GameObject explosionEffect;
+
+    bool hasExploded = false;
+
+    private void Start()
+    {
+        Explode();
+    }
+
+    private void Explode()
+    {
+        Debug.Log("BOOM!");
+
+        var psMain = Instantiate(explosionEffect, transform.position, transform.rotation).GetComponent<ParticleSystem>().main;
+        psMain.stopAction = ParticleSystemStopAction.Destroy;
+        
+
+        Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach(Collider nearbyObject in collidersToDestroy)
+        {
+            Destructible dest = nearbyObject.GetComponent<Destructible>();
+            if(dest != null)
+            {
+                dest.Destroy();
+            }
+        }
+
+        Collider[] collidersToMove = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach(Collider nearbyObject in collidersToMove)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+        Gizmos.DrawSphere(transform.position, explosionRadius);
+    }
+}
---
+++ By-Community/Brackeys - Intermediate/How to Make Slow Motion/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float slowdownFactor = .05f;
+    public float slowdownLength = 2f;
+
+    private void Update()
+    {
+        Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+    }
+
+    internal void DoSlowmotion()
+    {
+        Time.timeScale = slowdownFactor;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+}
