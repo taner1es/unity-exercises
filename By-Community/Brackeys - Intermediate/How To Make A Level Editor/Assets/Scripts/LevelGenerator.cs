fc7d95c1cc809fbf00278c8942707edaac0d9132
taner1es <taner.esmrglu@gmail.com>
2020-05-11 17:50:08 +0300

    Brackeys Intermediate - How to make a LEVEL EDITOR - project created - created, levelgenerator - imported, sprites (player, ground, coin) - created, pixel size designed level in krita - created, particle fx for collecting coin - created, scripts: level generator, simple player controller, coin collector

---
+++ By-Community/Brackeys - Intermediate/How To Make A Level Editor/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+    public ColorToPrefab[] colorMappings;
+
+    private void Start()
+    {
+        GenerateLevel();
+    }
+
+    private void GenerateLevel()
+    {
+        for(int x = 0; x < map.width; x++)
+        {
+            for(int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y);
+            }
+        }
+    }
+
+    private void GenerateTile(int x, int y)
+    {
+        Color pixelColor = map.GetPixel(x, y);
+
+        // ignore transparent pixels
+        if (pixelColor.a == 0)
+            return;
+
+        foreach(ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Instantiate(colorMapping.prefab, new Vector2(x, y), Quaternion.identity, transform);
+            }
+        }
+        
+    }
+}
---
+++ By-Community/Brackeys - Intermediate/How To Make A Level Editor/Assets/Scripts/SimpleCharacterController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharacterController : MonoBehaviour
+{
+    int direction;
+    bool jump;
+
+    public float walkSpeed = .2f;
+    public float jumpSpeed = .2f;
+
+    private void Update()
+    {
+        direction = 0;
+        jump = false;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1; 
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -1; 
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = 0;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jump = true;
+        }
+
+        if(direction != 0 || jump)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(direction * walkSpeed, jump ? jumpSpeed : 0);
+            if (direction == -1)
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            else
+                transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
