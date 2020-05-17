67d98fc0c285b79b932a2d2066249dc9e3c4fc66
taner1es <taner.esmrglu@gmail.com>
2020-05-17 21:52:34 +0300

    Brackeys Intermediate - Create Your Own Renderer Without Code - project created - installed, Lightweight RP package - created, navmesh plane & obstacles & agents - added, randomized patrolling functionality to agents - created & adjusted, custom forward renderer - created, a new layer for characters and removed from default forward renderer and added to it as a renderer feature - created, dithering shader & material - overrided, rendering material of the game objects layered as Character with a kind of a dithering shader

---
+++ ByCommunity/BrackeysIntermediate/CreateYourOwnRenderer/Assets/Scripts/Patrol.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] points;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        foreach(Transform point in points)
+        {
+            point.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to randomized destination
+        agent.destination = points[Random.Range(0, 19)].position;
+
+    }
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (agent.isOnNavMesh)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                GotoNextPoint();
+        }
+    }
+}
