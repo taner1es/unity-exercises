3e7d3e2b955822fe6125f6b9c3b819224e9041ac
taner1es <taner.esmrglu@gmail.com>
2020-05-30 19:32:11 +0300

    Brackeys Intermediate - Rider - project created - imported, post processing package - added, vignette & color grading effects - imported, ::Custom 2D Collider:: updated version by brackeys (original:https://github.com/GuyQuad/Custom-2D-Colliders) - created & added, a material with emission & bloom pp effect activated, instead of using ::MK Glow:: shaders (::MK Glow:: package is not in use in this project due to its priced now) - created, rider game object with components: physics, sprite, controller script - imported & added, cinemachine to follow rider - created, rotatable & weight sensitive line platform type

---
+++ ByCommunity/BrackeysIntermediate/Rider/Assets/Scripts/CarController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float speed = 20f;
+    public float rotationSpeed = 2f;
+
+    bool move = false;
+    bool isGrounded = false;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        move = Input.GetMouseButton(0);
+    }
+
+    private void FixedUpdate()
+    {
+        if(move)
+        {
+            if (isGrounded)
+                rb.AddForce(Vector2.right * speed * Time.fixedDeltaTime * 100, ForceMode2D.Force);
+            else
+                rb.AddTorque(rotationSpeed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+        }
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D()
+    {
+        isGrounded = false;
+    }
+}
