9055c9190be4f8289349658db43aebf2f53d9b39
taner1es <taner.esmrglu@gmail.com>
2020-06-02 03:09:02 +0300

    Brackeys Intermediate - Perlin Noise - project created - adjusted, background to black - created, Quad object with unlit shader - created, script to generate a brand new texture by using perlin noise function

---
+++ ByCommunity/BrackeysIntermediate/PerlinNoiseProcGen/Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PerlinNoise : MonoBehaviour
+{
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 20f;
+
+    public float offsetX = 0f;
+    public float offsetY = 0f;
+
+    private void Start()
+    {
+        offsetX = UnityEngine.Random.Range(0f, 999999f);
+        offsetY = UnityEngine.Random.Range(0f, 999999f);
+    }
+
+    private void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = GenerateTexture();
+    }
+
+    private Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        texture.Apply();
+        return texture;
+    }
+
+    private Color CalculateColor(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
+    }
+}
