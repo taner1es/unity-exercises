01ed40bbc8d62a7318c244dceb0a665820d9d339
taner1es <taner.esmrglu@gmail.com>
2020-06-03 02:50:52 +0300

    Brackeys Intermediate - MultipleTargetCamera - project created - created, a simple platform scene for testing purpose - created, controllable players (control 2 player with 1 keyboard, by WASD & Arrow Keys)

---
+++ ByCommunity/BrackeysIntermediate/MultipleTargetCamera/Assets/Scripts/MultipleTargetCamera.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleTargetCamera : MonoBehaviour
+{
+    public List<Transform> targets;
+    public Vector3 offset;
+    public float smoothTime = .5f;
+    public float minZoom = 40;
+    public float maxZoom = 10;
+    public float zoomLimiter = 40;
+
+    private Vector3 velocity = Vector3.zero;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        if (targets.Count == 0)
+            return;
+
+        Move();
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
+    }
+
+    private void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 newPos = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
+    }
+
+    private Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+            return targets[0].position;
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for(int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center;
+    }
+
+    private void OnDrawGizmos()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        Gizmos.DrawWireCube(bounds.center, bounds.size);
+    }
+}
