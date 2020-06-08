3814585062faf6c42335cb0499075ca8adcbb61c
taner1es <taner.esmrglu@gmail.com>
2020-06-08 10:59:53 +0300

    Brackeys Intermediate - Bomberman - project created - imported, starter assets which is brackeys prepared for the tutorial (includes:sprites & tiles) - created, background tilemap - created, gameplay tilemap & colliders - created, bomb & bomb spawner - created, scripts: bomb spawner, bomb(count down timer), bomb destroyer - extra: added an animation event to destroy explosion fx object from scene hierarchy

---
+++ ByCommunity/BrackeysIntermediate/Bomberman/Assets/Scripts/MapDestroyer.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapDestroyer : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+    public Tile wallTile;
+    public Tile destructibleTile;
+
+    public GameObject explosionPrefab;
+
+    public void Explode(Vector2 worldPos)
+    {
+        Vector3Int originCell = tilemap.WorldToCell(worldPos);
+        
+        ExplodeCell(originCell);
+
+        if(ExplodeCell(originCell + new Vector3Int(1, 0, 0)))
+        {
+            ExplodeCell(originCell + new Vector3Int(2, 0, 0));
+        }
+        
+        if(ExplodeCell(originCell + new Vector3Int(-1, 0, 0)))
+        {
+            ExplodeCell(originCell + new Vector3Int(-2, 0, 0));
+        }
+        
+        if(ExplodeCell(originCell + new Vector3Int(0, 1, 0)))
+        {
+            ExplodeCell(originCell + new Vector3Int(0, 2, 0));
+        }
+        
+        if(ExplodeCell(originCell + new Vector3Int(0, -1, 0)))
+        {
+            ExplodeCell(originCell + new Vector3Int(0, -2, 0));
+        }
+    }
+
+    bool ExplodeCell(Vector3Int cell)
+    {
+        Tile tile = tilemap.GetTile<Tile>(cell);
+
+        // no change for walls
+        if (tile == wallTile)
+            return false;
+
+        // remove destructibles
+        if(tile == destructibleTile)
+        {
+            tilemap.SetTile(cell, null);
+        }
+
+        // Create an explosion fx
+        Vector3 pos = tilemap.GetCellCenterWorld(cell);
+        Instantiate(explosionPrefab, pos, Quaternion.identity);
+
+        return true;
+    }
+}
