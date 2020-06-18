b8bf89c9e00b4946a9767d88386520b359802e4c
taner1es <taner.esmrglu@gmail.com>
2020-06-18 15:59:01 +0300

    Brackeys Advanced - How To Make Custom Inspector - project created - created, cube (coloring sample) & sphere (resizing sample) - created, behavior & editor scripts for both

---
+++ ByCommunity/BrackeysAdvanced/HowToMakeCustomInspector/Assets/Sphere.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public float baseSize = 1f;
+
+    void Update()
+    {
+        float animation = baseSize + Mathf.Sin(Time.time * 8f) * baseSize / 7f;
+        transform.localScale = Vector3.one * animation;
+    }
+}
