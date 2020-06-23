b0cf513c154bd6581f3423a2a96ed96410127bde
taner1es <taner.esmrglu@gmail.com>
2020-06-23 16:02:25 +0300

    Brackeys Advanced - Object Pooling - project created - created, sample for nonpooling cube spawner to check out the performance - implemented, object pooling pattern and checked out the performance again

---
+++ ByCommunity/BrackeysAdvanced/ObjectPooling/Assets/Scripts/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour, IPooledObject
+{
+    public float upForce = 1f;
+    public float sideForce = .1f;
+
+    public void OnObjectSpawn()
+    {
+        // Randomize the force
+        float xForce = Random.Range(-sideForce, sideForce);
+        float yForce = Random.Range(upForce / 2, upForce);
+        float zForce = Random.Range(-sideForce, sideForce);
+        Vector3 force = new Vector3(xForce, yForce, zForce);
+
+        // Apply
+        GetComponent<Rigidbody>().velocity = force;
+    }
+}
